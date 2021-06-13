a75e3d1ac84c880f59cf6582c96fbca4ed4aae4c
Andrii Maliuta <quadr988@gmail.com>
2021-06-13 17:26:01 +0300

    REST

--- src/com/anma/GetPage.groovy
+++ src/com/anma/GetPage.groovy
@@ -1,20 +1,55 @@
 package com.anma
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import com.thoughtworks.xstream.core.util.Base64Encoder
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
+
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
 class GetPage {
 
-    final String CONF_URL = "https://bass.netcracker.com"
-    long pageId = 1065015088;    // set page ID
-    HttpRequest request = HttpRequest.newBuilder(
-//                URI.create("https://bassdevqa.netcracker.com/rest/api/content/230366342?expand=body.view"))
-            URI.create("${CONF_URL}/rest/api/content/${pageId}?expand=body.view,version"))
-            .headers("Authorization", "Basic TOKEN")
-            .GET().build();
+    static def getPage(id) {
+//        final String CONF_URL = "https://bass.netcracker.com"
+        final String CONF_URL = "http://localhost:8712"
+        id = 6324225
+        long pageId = id;    // set page ID
+        def TOKEN = Base64Encoder.newInstance().encode("admin:admin".bytes)
+        HttpRequest request = HttpRequest.newBuilder(
+                URI.create("${CONF_URL}/rest/api/content/${pageId}?expand=body.view,version"))
+                .headers("Authorization", "Basic ${TOKEN}")
+                .GET()
+                .build();
 //        HttpHeaders httpHeaders = HttpHeaders();
-    HttpClient client = HttpClient.newBuilder().build();
-    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-    
+        HttpClient client = HttpClient.newBuilder().build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        return response
+    }
+
+    static void main(String[] args) {
+        println(getPage(6324225).body())
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        JsonObject jsonObject = gson.fromJson(getPage().body(), JsonObject.class);
+        JsonElement id = jsonObject.get("id");
+        String title = jsonObject.get("title").getAsString();
+
+        JsonObject body = jsonObject.get("body").getAsJsonObject();
+        JsonObject bodyView = body.get("view").getAsJsonObject();
+        JsonElement bodyValue = bodyView.get("value");
+        long pageVersion = jsonObject.get("version").getAsJsonObject().get("number").getAsLong();
+
+        Document document = Jsoup.parse(bodyValue.getAsString());
+        Elements links = document.select("a");
+        links.forEach(link -> System.out.println(link.attr("href")));
+    }
+
 }
---
+++ src/com/anma/UpdatePage.groovy
@@ -0,0 +1,39 @@
+package com.anma
+
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
+class UpdatePage {
+
+    def updatePage(id) {
+
+        def pageVersion = GetPage.getPage(id).version()
+
+        String newBody = bodyValue.getAsString().replace("222", "333");
+        String updatedPageBody = "{\n" +
+                "    \"version\": {\n" +
+                "        \"number\": " + (pageVersion + 1) + "\n" +
+                "    },\n" +
+                "    \"title\": \"" + title + "\",\n" +
+                "    \"type\": \"page\",\n" +
+                "    \"body\": {\n" +
+                "        \"storage\": {\n" +
+                "            \"value\": \"" + newBody + "\",\n" +
+                "            \"representation\": \"storage\"\n" +
+                "        }\n" +
+                "    }\n" +
+                "}";
+        HttpRequest.BodyPublisher publisher = HttpRequest.BodyPublishers.ofString(updatedPageBody);
+        HttpRequest postReq = HttpRequest.newBuilder()
+                .uri(URI.create("https://bass.netcracker.com/rest/api/content/" + pageId))
+                .PUT(publisher)
+                .headers("Authorization", "Basic TOKEN")
+                .headers("Content-Type", "application/json")
+                .build();
+
+        HttpResponse<String> postResponse = client.send(postReq, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(postResponse);
+
+    }
+}
